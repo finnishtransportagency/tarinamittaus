f79882d2bc937b13f6c2d2fbf1363cd997deb76e
Alvar Wihuri <alvar.wihuri@cgi.com>
2023-02-02 12:35:05 +0200

    Add health check

---
+++ src/main/java/fi/tarina/tarinamittaus/Controller/HealthCheckController.java
@@ -0,0 +1,22 @@
+package fi.tarina.tarinamittaus.Controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(path = "/rest/health/")
+public class HealthCheckController {
+    private static Logger logger = LogManager.getLogger(HealthCheckController.class);
+
+    @GetMapping(path = "")
+    public ResponseEntity<String> healthCheck() {
+        logger.debug("Health check called");
+        return new ResponseEntity<>("healthy", HttpStatus.OK);
+    }
+}
