7b702a8101e1c154abbc7da70b7889be135a1726
Alvar Wihuri <alvar.wihuri@gmail.com>
2024-09-26 18:17:08 +0300

    simplify store objects

--- src/main/app/src/stores/AsennettuAnturiStore.tsx
+++ src/main/app/src/stores/AsennettuAnturiStore.tsx
@@ -1,35 +1,10 @@
-import { makeObservable, observable } from "mobx";
 import MittausSuuntaTypeEnum from "../types/enums/mittausSuuntaType.enum";
 import { IAnturikohtaisetTunnusarvo } from "../types/interfaces/anturikohtaisetTunnusarvot.interface";
 import IAsennettuAnturi from "../types/interfaces/asennettuAnturi.interface";
 import AsennuspaikanTyyppiStore from "./AsennuspaikanTyyppiStore";
 
 
-export default class AsennettuAnturiStore implements IAsennettuAnturi {
-  malli = '';
-  gps_lat = 0;
-  gps_long = 0;
-  etaisyys_radasta_jos_eri = 0;
-  kerros = 0;
-  sijoituspaikan_lisaselite = '';
-  asennuspaikanTyyppi = new AsennuspaikanTyyppiStore();
-  anturikohtaisetTunnusarvot = tunnusArvot;
-
-  constructor() {
-    makeObservable(this, {
-      malli: observable,
-      gps_lat: observable,
-      gps_long: observable,
-      etaisyys_radasta_jos_eri: observable,
-      kerros: observable,
-      sijoituspaikan_lisaselite: observable,
-      asennuspaikanTyyppi: observable,
-      anturikohtaisetTunnusarvot: observable
-    })
-  }
-}
-
-let tunnusArvot: IAnturikohtaisetTunnusarvo[] = [
+const tunnusArvot: IAnturikohtaisetTunnusarvo[] = [
   {
     mittaussuunta_xyz: MittausSuuntaTypeEnum.X,
     tarinan_maksimiarvo: 0,
@@ -49,3 +24,13 @@ let tunnusArvot: IAnturikohtaisetTunnusarvo[] = [
     tarinan_tunnusluku_vw95_rms: 0,
   }
 ];
+export default class AsennettuAnturiStore implements IAsennettuAnturi {
+  malli = '';
+  gps_lat = 0;
+  gps_long = 0;
+  etaisyys_radasta_jos_eri = 0;
+  kerros = 0;
+  sijoituspaikan_lisaselite = '';
+  asennuspaikanTyyppi = new AsennuspaikanTyyppiStore();
+  anturikohtaisetTunnusarvot = tunnusArvot;
+}
--- src/main/app/src/stores/AsennuspaikanTyyppiStore.tsx
+++ src/main/app/src/stores/AsennuspaikanTyyppiStore.tsx
@@ -1,17 +1,8 @@
-import { makeObservable, observable } from "mobx";
 import SeliteTypeEnum from "../types/enums/seliteType.enum";
 import IAsennuspaikantyyppi from "../types/interfaces/asennuspaikanTyyppi.interface";
-
 
 
 export default class AsennuspaikanTyyppiStore implements IAsennuspaikantyyppi {
   selite = SeliteTypeEnum.muu;
   lisatiedot = '';
-
-  constructor() {
-    makeObservable(this, {
-      selite: observable,
-      lisatiedot: observable
-    })
-  }
 }
