2a043e00d433818cd58ebd7f5d88cdbd174f8943
Alvar Wihuri <alvar.wihuri@gmail.com>
2024-09-26 18:20:14 +0300

    remove mobx

--- src/main/app/src/views/MittausView.tsx
+++ src/main/app/src/views/MittausView.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
-import { observer } from 'mobx-react';
-
 import MittausForm from './MittausForm';
 import Container from 'react-bootstrap/Container';
 
 
-const MittausView = observer(() => {
+const MittausView = () => {
   return (
     <Container fluid>
       <MittausForm />
     </Container>
   )
-})
+}
 export default MittausView;
