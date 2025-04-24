cc765929a54bacc72b15430ab620e6fd43afa711
Alvar Wihuri <alvar.wihuri@gmail.com>
2025-04-24 12:45:27 +0300

    remove axios

--- src/main/app/src/api.ts
+++ src/main/app/src/api.ts
@@ -4,65 +4,81 @@ const baseUrl = fullRestURL();
 
 export const postData = async (data = {}) => {
   const response = await fetch(baseUrl, {
-    method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
+    method: "POST",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    redirect: 'follow',
-    referrerPolicy: 'no-referrer',
-    body: JSON.stringify(data)
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify(data),
   });
   return response.json();
-}
+};
 
 export const putData = async (data = {}) => {
   return fetch(baseUrl, {
-    method: 'PUT',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
+    method: "PUT",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    redirect: 'follow',
-    referrerPolicy: 'no-referrer',
-    body: JSON.stringify(data)
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify(data),
   });
-}
+};
 
 export const getData = async (id: string) => {
   return fetch(baseUrl + id, {
-    method: 'GET',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
+    method: "GET",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-    redirect: 'follow',
-    referrerPolicy: 'no-referrer',
-  }).then(data => data.json())
-    .then(json => json.status && json.status !== 200 ? null : json)
-    .catch(err => {
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+  })
+    .then((data) => data.json())
+    .then((json) => (json.status && json.status !== 200 ? null : json))
+    .catch((err) => {
       console.error(err);
       return null;
     });
-}
+};
 
 export const deleteData = async (id: string) => {
   const response = await fetch(baseUrl + id, {
-    method: 'DELETE',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
+    method: "DELETE",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-    redirect: 'follow',
-    referrerPolicy: 'no-referrer',
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
   });
   return response.json();
-}
+};
+
+export const getList = async () => {
+  const response = await fetch(baseUrl, {
+    method: "GET",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+  });
+  return response.json();
+};
